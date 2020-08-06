3d54b94c301d287d49364f25c38edfc9b6ddd8b2
Himansh <himanshsharma0622@gmail.com>
2020-08-06 13:13:26 +0530

    day -1

---
+++ Day-1/binary_search.cpp
@@ -0,0 +1,22 @@
+int bin_search(int A[], int left, int right, int k)
+{
+    int mid;
+while(left<=right)
+{
+    mid=(left+right)/2;
+    if(A[mid]==k)
+    {
+      
+        return mid;
+        
+    }
+    if(A[mid]<k)
+    {
+        left=mid+1;
+    }
+    else{
+        right=mid-1;
+    }
+}
+return -1;
+}
