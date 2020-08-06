40aa626cec4b3ca831bb204372ebc71a562edfd2
Himansh <himanshsharma0622@gmail.com>
2020-08-06 13:24:13 +0530

    day -1

--- Day-1/binary_search.cpp
+++ Day-1/binary_search.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 int bin_search(int A[], int left, int right, int k)
 {
     int mid;
@@ -7,7 +10,7 @@ while(left<=right)
     if(A[mid]==k)
     {
       
-        return mid;
+        return 1;
         
     }
     if(A[mid]<k)
@@ -19,4 +22,22 @@ while(left<=right)
     }
 }
 return -1;
+}
+
+int main() {
+	int t;
+	cin>>t;
+	for(int k=0;k<t;k++)
+	{
+	    int n,l,i;
+	    cin>>n>>l;
+	    int arr[n];
+	    for(i=0;i<n;i++)
+	    {
+	        cin>>arr[i];
+	    }
+	    int ans=bin_search(arr,0,n,l);
+	    cout<<ans<<endl;
+	}
+	return 0;
 }
