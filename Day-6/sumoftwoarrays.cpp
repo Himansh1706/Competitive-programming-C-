51b3e7d80948515f7ed53f7943b46648cf9c18df
Himansh <himanshsharma0622@gmail.com>
2020-08-21 05:47:38 +0530

    easy

---
+++ Day-6/sumoftwoarrays.cpp
@@ -0,0 +1,51 @@
+/* 
+Take as input N, the size of array. Take N more inputs and store that in an array. Take as input M, the size of second array and take M more inputs and store that in second array. Write a function that returns the sum of two arrays. Print the value returned.
+
+Input Format
+Constraints
+Length of Array should be between 1 and 1000.
+
+Output Format
+Sample Input
+4
+1 0 2 9
+5
+3 4 5 6 7
+Sample Output
+3, 5, 5, 9, 6, END
+Explanation
+Sum of [1, 0, 2, 9] and [3, 4, 5, 6, 7] is [3, 5, 5, 9, 6] and the first digit represents carry over , if any (0 here ) .
+*/
+
+
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main() {
+	string s;
+	cin>>s;
+	int n=s.length();
+	char c;
+	int maxx=0;
+	for(int i=0;i<n;i++)
+	{
+		char a=s[i];
+		int count=0;
+		for(int j=i+1;j<n;j++)
+		{
+			if(s[j]==a)
+			{
+				count++;
+			}
+		}
+		if(count>=maxx)
+		{
+			c=a;
+			maxx=count;
+		}
+	}
+	cout<<c<<endl;
+
+	return 0;
+} 
