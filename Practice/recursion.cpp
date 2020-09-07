83ec617b7e34c5ad698297fc86e04278ae2cefdf
Himansh <himanshsharma0622@gmail.com>
2020-09-07 18:50:37 +0530

    power

--- Practice/recursion.cpp
+++ Practice/recursion.cpp
@@ -50,6 +50,26 @@ void all_occurences(int *a,int i,int n,int key)
     all_occurences(a,i+1,n,key);
 
 }
+int fast_power(int a,int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    int b=fast_power(a,n/2);
+    int p=b*b;
+    if(n%2==0)
+    {
+        return p;
+    }
+    else {
+        return p*a;
+    }
+}
+int power(int a,int n)
+{
+    return n==0?1:a*power(a,n-1);
+}
 int main()
 {
 int arr[]={1,2,3,4,5,6,7,8,9,100,11,12,13,14,15,16,17,18,19,100,100,22,23,24};
@@ -57,5 +77,8 @@ int n=24;
 cout<<linear_search(arr,n,100)+1<<endl;
 cout<<last_occurence(arr,n,100)+1<<endl;
 all_occurences(arr,0,n,100);
+cout<<endl;
+cout<<fast_power(5,2)<<endl;
+cout<<power(5,2)<<endl;
 return 0;
 }
